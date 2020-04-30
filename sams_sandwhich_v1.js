e49b87f40ee9a19a85144ceae81ad03e9e37cc6d
noahelisia <63981087+noahelisia@users.noreply.github.com>
2020-05-01 10:09:56 +1200

    Update sams_sandwhich_v1.js

--- sams_sandwhich_v1.js
+++ sams_sandwhich_v1.js
@@ -1,7 +1,14 @@
 // JavaScript Document
-alert("js is attached")
+// JavaScript Document
+//global variables
+var database = firebase.database();//creating the link to your database
+var orderRef = database.ref('order');//creates a referene point to firebase, users node 
 
-function loopform(form) {
+
+
+
+function loopForm(form){
+	submit_button.enable;
 	alert("Check your order please");//test alert
 	var sandwhichOrder = []; //defines an empty list
 	for ( var i = 0; i < form.elements.length; i++ ) {//for loop through the form 
@@ -17,8 +24,41 @@ function loopform(form) {
 				alert(sandwhichOrder);//test alert
 			}
 		}
+	}
+	checkInputs(sandwhichOrder);
+	}
 		//once the for loop has gone through each element in the form 
 		//it will output the list using the DOM
-		document.getElementById( "radioResults").innerHTML = sandwhichOrder;
-	}
-}
+		function checkInputs(sandwhichOrder){
+			var customer_name = customer_name.value;
+			var customer_number = customer_number.value;
+			alert("in check name function");
+			if(document.getElementById("userName").validity.patternMismatch){
+				alert("please enter a valid name")
+				return;
+			}
+			if(document.getElementById("usercellPhone").validity.patternMismatch){
+				alert("please enter a valid number");
+				return;
+			}else{
+				alert(customer_name + customer_number);
+				document.getElementById("radioResults").innerHTML="name: " + customer_name+""+"phone_number"+customer_number;
+				document.getElementById("radioResults").innerHTML="sandWhich order:" + sandwhichOrder;
+				sendData(customer_name,customer_number,sandwhichOrder);
+			}
+		}
+		function sendData(user_name,usercellPhone,sandwhichOrder){
+		var data={
+			sandwhich:sandwhichOrder,
+			name:user_name,
+			cell_phone:usercellPhone
+			}
+		orderRef.push(data);
+			alert("data has been pushed");
+			document.getElementById('confirmOverlay').style.height="100%";
+			setTimeout(function(){
+				location.reload();
+			},3000);
+			submit_button.disable
+		
+		}
